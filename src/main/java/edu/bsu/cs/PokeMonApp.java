42cc9f5bb497f2f1e7ece7272b80c865a4fb28d1
Dawson Vaal <dcvaal@bsu.edu>
2021-10-22 14:44:46 -0400

    Refactor PokeMonApp into an App and LocationWidget

--- src/main/java/edu/bsu/cs/PokeMonApp.java
+++ src/main/java/edu/bsu/cs/PokeMonApp.java
@@ -1,24 +1,10 @@
 package edu.bsu.cs;
 
 import javafx.application.Application;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-
 public class PokeMonApp extends Application {
-    private final Executor executor = Executors.newSingleThreadExecutor();
-    private final Runnable revisionTask = new locationTask();
-
-    private final TextField userInput;
-    private final Button searchLocationsButton;
-    private final TextArea locationOutput;
 
     @Override
     public void start(Stage primaryStage) {
@@ -27,45 +13,5 @@ public class PokeMonApp extends Application {
         primaryStage.show();
     }
 
-    public PokeMonApp() {
-        userInput = new TextField();
-        searchLocationsButton = new Button("Search Locations");
-        locationOutput = new TextArea();
-        locationOutput.setPrefHeight(600);
-        locationOutput.setEditable(false);
-        searchLocationsButton.setOnAction(event -> executor.execute(revisionTask));
-    }
 
-    private Parent LocationsGUI() {
-        VBox vBox = new VBox();
-        vBox.setPrefSize(1280, 720);
-        vBox.getChildren().add(userInput);
-        vBox.getChildren().add(searchLocationsButton);
-        vBox.getChildren().add(locationOutput);
-        return vBox;
-    }
-
-    private final class locationTask implements Runnable {
-
-        @Override
-        public void run() {
-            disableEditing();
-            pauseThreadFor1Second();
-            String locations = "location";
-            locationOutput.setText(locations);
-            enableEditing();
-        }
-
-        private void pauseThreadFor1Second () {
-            try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
-        }
-            private void enableEditing() {
-            userInput.setEditable(true);
-            searchLocationsButton.setDisable(false);
-        }
-        private void disableEditing() {
-            userInput.setEditable(false);
-            searchLocationsButton.setDisable(true);
-        }
-    }
 }
