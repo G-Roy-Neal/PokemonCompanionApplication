73a5e8acdcf226ec156edcfa0bb17712232f39ab
“Wesley <wjkring@bsu.edu>
2021-10-22 14:03:28 -0400

    Finish PokemonLocationBuilder class

--- src/main/java/edu/bsu/cs/PokemonLocationBuilder.java
+++ src/main/java/edu/bsu/cs/PokemonLocationBuilder.java
@@ -4,6 +4,8 @@ import com.jayway.jsonpath.JsonPath;
 import net.minidev.json.JSONArray;
 
 import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,17 +16,27 @@ public class PokemonLocationBuilder {
         this.inputData = inputData;
     }
 
-    public List<PokemonLocation> buildLocationList (){
+    public List<PokemonLocation> buildLocationList () throws IOException {
         List<PokemonLocation> list = new ArrayList<>();
-        list.add(new PokemonLocation(""));
+        String locationUrl = getUrl();
+        URL Url = new URL(locationUrl);
+        URLConnection connection = Url.openConnection();
+        InputStream rawLocations = connection.getInputStream();
+        JsonPath locationPath = JsonPath.compile("$..location_area..name");
+        JSONArray locationsArray = locationPath.read(rawLocations);
+        for (Object o : locationsArray) {
+            list.add(new PokemonLocation(o.toString()));
+        }
         return list;
+
     }
 
     public String getUrl() throws IOException {
-        ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
-        inputData.transferTo(byteArray);
-        JsonPath timePath = JsonPath.compile("$..location_area_encounters");
-        JSONArray locationUrl = timePath.read(byteArray);
+        ByteArrayOutputStream temporaryByteArray = new ByteArrayOutputStream();
+        inputData.transferTo(temporaryByteArray);
+        InputStream clone = new ByteArrayInputStream(temporaryByteArray.toByteArray());
+        JsonPath encountersPath = JsonPath.compile("$..location_area_encounters");
+        JSONArray locationUrl = encountersPath.read(clone);
         return locationUrl.get(0).toString();
     }
 }
