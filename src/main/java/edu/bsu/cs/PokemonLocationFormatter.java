e0d91882dd3b198dd7695f69eeb051739264aceb
Gavin Neal <grneal@bsu.edu>
2021-10-22 15:00:45 -0400

    Add new test cases to PokemonLocationFormatterTest

--- src/main/java/edu/bsu/cs/PokemonLocationFormatter.java
+++ src/main/java/edu/bsu/cs/PokemonLocationFormatter.java
@@ -3,8 +3,20 @@ package edu.bsu.cs;
 public class PokemonLocationFormatter {
 
 
-     public String formatLocation(String rawLocationDescription) {
-         return "Route 203 area";
+     public String transformLocationDescription(String rawLocation) {
+         if (checkIfLocationInGame(rawLocation).equals(true)) {
+             return formatLocationDescription(rawLocation);
+         }
+         return null;
+    }
+
+    private String formatLocationDescription(String rawLocation) {
+         rawLocation = rawLocation.replace('-',' ');
+         return rawLocation.replace("sinnoh r", "R");
+    }
+
+    private Object checkIfLocationInGame(String rawLocation) {
+        return rawLocation.contains("sinnoh");
     }
 
 }
--- src/test/java/edu/bsu/cs/PokemonLocationFormatterTest.java
+++ src/test/java/edu/bsu/cs/PokemonLocationFormatterTest.java
@@ -1,16 +1,22 @@
 package edu.bsu.cs;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class PokemonLocationFormatterTest {
 
-    @Test
-    public void testPokemonLocationFormatter(){
-        PokemonLocationFormatter formatter = new PokemonLocationFormatter();
-        String realLocation = "Route 203 area";
-        String testLocation = formatter.formatLocation("sinnoh-route-203-area");
-        Assertions.assertEquals(realLocation,testLocation);
 
+    @ParameterizedTest
+    @CsvSource({
+            "sinnoh-route-203-area, Route 203 area",
+            "hoenn-route-102-area,",
+            "sinnoh-route-204-south-towards-jubilife-city, Route 204 south towards jubilife city"
+    })
+    public void testFormatLocation(String location, String expected) {
+        PokemonLocationFormatter formatter = new PokemonLocationFormatter();
+        String result = formatter.transformLocationDescription(location);
+        Assertions.assertEquals(expected, result);
     }
+
 }
