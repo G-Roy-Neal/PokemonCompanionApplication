40b7485be47bb60d9390563ef5baa366d249d2d7
Gavin Neal <grneal@bsu.edu>
2021-10-28 16:58:18 -0400

    Add new features to GUI

--- src/main/java/edu/bsu/cs/PokemonApp.java
+++ src/main/java/edu/bsu/cs/PokemonApp.java
@@ -4,11 +4,15 @@ import edu.bsu.cs.locations.PokemonLocation;
 import edu.bsu.cs.locations.PokemonLocationBuilder;
 import edu.bsu.cs.locations.PokemonLocationFormatter;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -21,9 +25,16 @@ import java.util.concurrent.Executors;
 public class PokemonApp extends Application {
     private final Executor executor = Executors.newSingleThreadExecutor();
     private final Runnable revisionTask = new locationTask();
+
     private final TextField userInput;
-    private final Button searchLocationsButton;
+    private final TextField pokemonName;
+    private final TextField pokemonHeight;
+    private final TextField pokemonType;
+    private final TextField pokemonWeight;
+    private final ComboBox<String> dropdownMenu;
     private final TextArea locationOutput;
+    private final TextArea image;
+    private final Button searchButton;
 
     @Override
     public void start(Stage primaryStage) {
@@ -33,21 +44,50 @@ public class PokemonApp extends Application {
     }
 
     public PokemonApp() {
-        userInput = new TextField();
-        searchLocationsButton = new Button("Search Locations");
+        ObservableList<String> comboBoxArrayList = FXCollections.observableArrayList("Locations", "Moves", "Type Advantages");
+        userInput = new TextField("Search");
+        pokemonName = new TextField("Name");
+        pokemonHeight = new TextField("Height");
+        pokemonType = new TextField("Type");
+        pokemonWeight = new TextField("Weight");
+        dropdownMenu = new ComboBox<>(comboBoxArrayList);
+        searchButton = new Button("\uD83D\uDD0E");
         locationOutput = new TextArea();
-        locationOutput.setPrefHeight(600);
+        image = new TextArea("Image");
+
         locationOutput.setEditable(false);
-        searchLocationsButton.setOnAction(event -> executor.execute(revisionTask));
+        searchButton.setOnAction(event -> executor.execute(revisionTask));
     }
 
     private Parent LocationsGUI() {
-        VBox vBox = new VBox();
-        vBox.setPrefSize(1280, 720);
-        vBox.getChildren().add(userInput);
-        vBox.getChildren().add(searchLocationsButton);
-        vBox.getChildren().add(locationOutput);
-        return vBox;
+        HBox parentBox = new HBox();
+        HBox nameHeightBox = new HBox();
+        HBox typeWeightBox = new HBox();
+        HBox querySearchButtonBox = new HBox();
+        VBox dropDownOutputBox = new VBox();
+        VBox searchImageDataBox = new VBox();
+
+        parentBox.getChildren().add(dropDownOutputBox);
+        parentBox.getChildren().add(searchImageDataBox);
+
+        dropDownOutputBox.getChildren().add(dropdownMenu);
+        dropDownOutputBox.getChildren().add(locationOutput);
+
+        searchImageDataBox.getChildren().add(querySearchButtonBox);
+        searchImageDataBox.getChildren().add(image);
+        searchImageDataBox.getChildren().add(nameHeightBox);
+        searchImageDataBox.getChildren().add(typeWeightBox);
+
+        querySearchButtonBox.getChildren().add(userInput);
+        querySearchButtonBox.getChildren().add(searchButton);
+
+        nameHeightBox.getChildren().add(pokemonName);
+        nameHeightBox.getChildren().add(pokemonHeight);
+
+        typeWeightBox.getChildren().add(pokemonType);
+        typeWeightBox.getChildren().add(pokemonWeight);
+
+        return parentBox;
     }
 
     private final class locationTask implements Runnable {
@@ -74,12 +114,12 @@ public class PokemonApp extends Application {
 
         private void enableEditing() {
             userInput.setEditable(true);
-            searchLocationsButton.setDisable(false);
+            searchButton.setDisable(false);
         }
 
         private void disableEditing() {
             userInput.setEditable(false);
-            searchLocationsButton.setDisable(true);
+            searchButton.setDisable(true);
         }
     }
 }
