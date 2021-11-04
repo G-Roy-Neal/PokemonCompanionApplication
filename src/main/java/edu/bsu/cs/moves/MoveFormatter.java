51f9cbb73136846b793424a42ecebeedc3ae92de
Gavin Neal <grneal@bsu.edu>
2021-11-04 17:15:02 -0400

    Add MoveFormatter class and the respective test cases

---
+++ src/main/java/edu/bsu/cs/moves/MoveFormatter.java
@@ -0,0 +1,8 @@
+package edu.bsu.cs.moves;
+
+public class MoveFormatter {
+
+    public String format(PokemonMove move) {
+        return "20, Hyperbeam";
+    }
+}
---
+++ src/test/java/edu/bsu/cs/MoveFormatterTest.java
@@ -0,0 +1,33 @@
+package edu.bsu.cs;
+
+import edu.bsu.cs.moves.MoveFormatter;
+import edu.bsu.cs.moves.PokemonMove;
+import edu.bsu.cs.moves.PokemonMoveBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MoveFormatterTest {
+
+    @Test
+    public void testBuildMove(){
+        MoveFormatter moveFormatter = new MoveFormatter();
+        PokemonMove move1 = new PokemonMove.Builder().withName("Hyperbeam").withLevel(20).build();
+        Assertions.assertEquals("20, Hyperbeam", moveFormatter.format(move1));
+    }
+
+
+    @Test
+    public void testBuildFormattedOutput() throws IOException {
+        InputStream testingData = Thread.currentThread().getContextClassLoader().getResourceAsStream("ralts-test.json");
+        assert testingData != null;
+        PokemonMoveBuilder moveBuilder = new PokemonMoveBuilder(testingData);
+        String expectedMoves = """
+                This Pokemon will learn at level
+                20, Hyperbeam
+                This pokemon has no moves it can be taught.
+                """;
+    }
+}
