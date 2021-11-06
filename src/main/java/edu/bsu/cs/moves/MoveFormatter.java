e2cd3ba06d39c52ceea982a5934bbc78dc79b0d2
Gavin Neal <grneal@bsu.edu>
2021-11-06 14:07:45 -0400

    Refactor MoveFormatter class to accept lists of moves

--- src/main/java/edu/bsu/cs/moves/MoveFormatter.java
+++ src/main/java/edu/bsu/cs/moves/MoveFormatter.java
@@ -1,12 +1,37 @@
 package edu.bsu.cs.moves;
 
-public class MoveFormatter {
+import java.util.ArrayList;
+import java.util.List;
 
-    public String format(PokemonMove move) {
-        if (move.getLevel() > 0) {
-            return "20, Hyperbeam";
-        }else {
-            return "Water Cannon";
+public class MoveFormatter {
+    private final List<PokemonMove> taughtMoves;
+    private final List<PokemonMove> movesThatWillBeLearned;
+
+    public MoveFormatter() {
+        taughtMoves = new ArrayList<>();
+        movesThatWillBeLearned = new ArrayList<>();
+    }
+
+    public StringBuilder formatTaughtMoves() {
+        StringBuilder taughtMovesString = new StringBuilder("These are the moves it can be taught:\n");
+        for (PokemonMove move: taughtMoves){
+            String formattedMove = String.format("%s\n", move.getName());
+            taughtMovesString.append(formattedMove);
+        }
+        return taughtMovesString;
+    }
+
+    private void classifyMoves(PokemonMove move){
+        if (discernIfTaught(move)) {
+            taughtMoves.add(move);
+        } else {
+            movesThatWillBeLearned.add(move);
         }
     }
+
+    private boolean discernIfTaught(PokemonMove move) {
+        return move.getLevel() > 0;
+    }
+
+
 }
