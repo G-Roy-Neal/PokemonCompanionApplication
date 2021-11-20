c2c9b6aca998cc779adb233b31e86ba5c94e826b
Dawson Vaal <dcvaal@bsu.edu>
2021-11-20 11:53:29 -0500

    Remove test in ImageBuilderTest to make it not dependant on the network.

--- src/test/java/edu/bsu/cs/ImageBuilderTest.java
+++ src/test/java/edu/bsu/cs/ImageBuilderTest.java
@@ -4,7 +4,6 @@ import edu.bsu.cs.PokemonImage.ImageBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -15,17 +14,5 @@ public class ImageBuilderTest {
         assert testingData != null;
         ImageBuilder imageBuilder = new ImageBuilder();
         Assertions.assertEquals("280", imageBuilder.getPokemonId(testingData));
-    }
-
-    @Test
-    public void testGetPokemonImage() throws IOException {
-        InputStream testingData = Thread.currentThread().getContextClassLoader().getResourceAsStream("Squirtle.png");
-        ByteArrayOutputStream testingByteArray = new ByteArrayOutputStream();
-
-        ImageBuilder imageBuilder = new ImageBuilder();
-        ByteArrayOutputStream imageByteArray = new ByteArrayOutputStream();
-
-        assert testingData != null;
-        Assertions.assertEquals(testingData.transferTo(testingByteArray), imageBuilder.getPokemonImage("007").transferTo(imageByteArray));
     }
 }
