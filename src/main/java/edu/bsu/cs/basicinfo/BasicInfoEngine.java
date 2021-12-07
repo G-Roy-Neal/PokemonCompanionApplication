1e3fc013d8dca1f026023f6073ba168afdabb12a
Wesley <wjkring@bsu.edu>
2021-12-07 15:07:43 -0500

    Remove unused interfaces for QueryEngine BasicInfoEngine and ImageEngine

--- src/main/java/edu/bsu/cs/basicinfo/BasicInfoEngine.java
+++ src/main/java/edu/bsu/cs/basicinfo/BasicInfoEngine.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-public interface BasicInfoEngine {
-    List<String> getBaisicInfo(InputStream inputData) throws IOException;
+public class BasicInfoEngine {
+    public List<String> getBaisicInfo(InputStream inputData) throws IOException {
+        BasicInfoReader infoBuilder = new BasicInfoReader(inputData);
+        BasicInfoFormatter infoFormatter = new BasicInfoFormatter(infoBuilder.buildBasicInfo());
+        return infoFormatter.formatInfo();
+    }
 }
